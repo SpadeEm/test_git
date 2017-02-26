2ae7c695191c98546da7136c4d0f794fbaa7b381
zyc <965702794@qq.com>
2017-02-26 15:45:45 +0800

    规范了代码

---
+++ test_2/src/test_2/roll.java
@@ -0,0 +1,14 @@
+package test_2;
+
+public class roll {
+	int a ;
+	int b ;
+	public static int add(int a,int b){
+		int x;
+		x= a + b;
+		return x;
+	}
+public static void main(String[] args) {
+	System.out.println(add(9,6));
+}
+}
