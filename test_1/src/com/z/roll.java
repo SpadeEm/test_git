6d2bbc89185791b03940fa78434643cb2a2f19a4
zyc <965702794@qq.com>
2017-02-26 15:35:06 +0800

    修改了 a  b 的值

---
+++ test_1/src/com/z/roll.java
@@ -0,0 +1,16 @@
+package com.z;
+
+public class roll {
+	int a =5;
+	int b =6;
+	public static int add(int a,int b){
+		int x;
+		x= a + b;
+		return x;
+	}
+public static void main(String[] args) {
+	System.out.println(add(5,7));
+}
+
+
+}
